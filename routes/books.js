a09a6ea786db02f0fd683097cfd8b232ec3a36aa
nahwinrajan <nahwin.rajan@gmail.com>
2017-01-09 20:37:34 +0700

    finish book-crud feauter; must put appropriate UI once autorization and authentication is up

--- routes/books.js
+++ routes/books.js
@@ -7,15 +7,14 @@ var Book = require("./../models/book");
 // variables - middleware
 
 // variable - helper
-var forbidden     = require('./../helpers/response/forbidden');
-var notFound      = require('./../helpers/response/notFound');
-var serverError   = require('./../helpers/response/serverError');
 
 // return all books
 router.get('/', function(req, res) {
-  Book.all((err, foundBooks) => {
+  Book.all(function (err, foundBooks) {
     if (err) {
-      serverError(err, req, res);
+      console.log("ERROR: " + (err.name) ? err.name : "");
+      console.log(err);
+      res.redirect("back");
     } else {
       res.render('books/index', { books: foundBooks });
     }
@@ -28,28 +27,36 @@ router.get('/new', function(req, res) {
 });
 
 // create new book listing
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   req.body.book.title           = req.sanitize(req.body.book.title);
   req.body.book.description     = req.sanitize(req.body.book.description);
   req.body.book.isbn            = req.sanitize(req.body.book.isbn);
 
-  Book.create(book, (err, createdBook) => {
+  Book.create(req.body.book, function (err, createdBook) {
     if(err) {
+      console.log("Error encountered: \n ------------- \n", err);
       req.flash("error", "Can't create book");
+      req.flash("error", err.message);
       res.redirect("back");
     } else {
       req.flash("success", createdBook.title + " created");
-      res.redirect("/" + createdBook.id);
+      res.redirect("/books/" + createdBook.id);
     }
   });
 });
 
 // show particular book
-router.get('/:id', (req, res) => {
-  Book.findById(req.params.id, (err, foundBook) => {
+router.get('/:id', function (req, res) {
+  Book.findById(req.params.id, function (err, foundBook) {
     if (err) {
+      console.log("ERROR: " + (err.name) ? err.name : "");
       console.log(err);
-      notFound(err, req, res);
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else if (foundBook === null) {
+      console.log("book not found: " + req.params.id);
+      req.flash("error", "The book you are searching is not found");
+      res.redirect("back");
     } else {
       res.render("books/show", { book: foundBook});
     }
@@ -57,11 +64,17 @@ router.get('/:id', (req, res) => {
 });
 
 // show edit form
-router.get('/:id/edit', (req, res) => {
-  Book.findById(req.params.id, (err, foundBook) => {
+router.get('/:id/edit', function (req, res) {
+  Book.findById(req.params.id, function (err, foundBook) {
     if (err) {
+      console.log("ERROR: " + (err.name) ? err.name : "");
       console.log(err);
-      notFound(err, req, res);
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else if (foundBook === null) {
+      console.log("book not found: " + req.params.id);
+      req.flash("error", "The book you are searching for is not found");
+      res.redirect("back");
     } else {
       res.render("books/edit", { book: foundBook});
     }
@@ -69,28 +82,38 @@ router.get('/:id/edit', (req, res) => {
 });
 
 // update book listing
-router.put("/:id", (req, res) => {
+router.put("/:id", function (req, res) {
   req.body.book.title           = req.sanitize(req.body.book.title);
   req.body.book.description     = req.sanitize(req.body.book.description);
   req.body.book.isbn            = req.sanitize(req.body.book.isbn);
 
-  Book.findByIdAndUpdate(req.params.id, req.body.book, (err, updatedBook) => {
+  Book.findByIdAndUpdate(req.params.id, req.body.book, function (err, updatedBook) {
     if(err) {
       req.flash("error", "Can't update book");
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else if (updatedBook === null) {
+      console.log("book not found: " + req.params.id);
+      req.flash("error", "The book you are trying to update is not found");
       res.redirect("back");
     } else {
       req.flash("success", updatedBook.title + " updated");
-      res.redirect("/" + updatedBook.id);
+      res.redirect("/books/" + updatedBook.id);
     }
   });
 });
 
 // delete book
-router.delete("/:id", (req, res) => {
+router.delete("/:id", function (req, res) {
   Book.findByIdAndRemove(req.params.id, function(err, deletedBook) {
     if(err) {
       console.log(err); //todo: change all error loggin to separate file
       req.flash("error", "Can't remove book");
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else if (deletedBook === null) {
+      console.log("book not found: " + req.params.id);
+      req.flash("error", "The book you are trying to delete is not found");
       res.redirect("back");
     } else {
       req.flash("success", deletedBook.title + " removed");
